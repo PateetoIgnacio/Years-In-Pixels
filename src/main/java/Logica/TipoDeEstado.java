2fe7c66f3438dd1f5dbb654af89d6643b39fbbf4
Patricio <pantinao.guairao@gmail.com>
2018-05-23 21:44:26 -0400

    se agregan comentarios

--- src/main/java/Logica/TipoDeEstado.java
+++ src/main/java/Logica/TipoDeEstado.java
@@ -2,10 +2,12 @@
 package Logica;
 
 public enum TipoDeEstado {
+    
+    /* desde el mejor al peor estado */
     ESTADO_1,
     ESTADO_2,
     ESTADO_3,
     ESTADO_4,
     ESTADO_5,
-    SIN_ESPECIFICAR
+    SIN_ESPECIFICAR 
 }

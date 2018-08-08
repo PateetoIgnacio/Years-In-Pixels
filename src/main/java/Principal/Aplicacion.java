7c2219b6bf2fd7dfe98400f9ee29fe916509457c
Patricio <pantinao.guairao@gmail.com>
2018-08-08 15:15:23 -0400

    Merge origin/master
    Conflicts:
    	src/main/java/Logica/AnhoEnPixeles.java
    	src/main/java/Principal/Aplicacion.java
    
    Cambios en la instancia.

--- src/main/java/Principal/Aplicacion.java
+++ src/main/java/Principal/Aplicacion.java
@@ -1,13 +1,10 @@
 
 package Principal;
 
-import Visual.Ventana;
 import Visual.VentanaInicioSesion;
 
 public class Aplicacion {
     public static void main(String[] args) {
-
-        Ventana ventana = new Ventana();
-        //VentanaInicioSesion vInSe = new VentanaInicioSesion();
+        VentanaInicioSesion vInSe = new VentanaInicioSesion();
     }
 }

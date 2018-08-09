2056e7dcf6d5e643ffb14c7800ce1702eacaea0e
Patricio <pantinao.guairao@gmail.com>
2018-08-09 13:19:27 -0400

    cambio en la declaracion

--- src/main/java/Principal/Aplicacion.java
+++ src/main/java/Principal/Aplicacion.java
@@ -4,9 +4,8 @@ package Principal;
 import Visual.VentanaInicioSesion;
 
 public class Aplicacion {
-    public static void main(String[] args) {
-        
-        VentanaInicioSesion vInSe = new VentanaInicioSesion();
+    public static void main(String[] args) {  
+        VentanaInicioSesion ventanaInicio = new VentanaInicioSesion();
 
     }
 }

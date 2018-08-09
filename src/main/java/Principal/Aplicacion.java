b52479cd13283703d214dd375f13d96f774fb867
Patricio <pantinao.guairao@gmail.com>
2018-08-09 08:26:48 -0400

    cambios definitivos a esta clase.

--- src/main/java/Principal/Aplicacion.java
+++ src/main/java/Principal/Aplicacion.java
@@ -1,15 +1,12 @@
 
 package Principal;
 
-import Visual.Ventana;
-import Visual.VentanaEleccionColoresPersonalizados;
 import Visual.VentanaInicioSesion;
 
 public class Aplicacion {
     public static void main(String[] args) {
 
         VentanaInicioSesion vInSe = new VentanaInicioSesion();
-        //VentanaEleccionColoresPersonalizados veColores = new VentanaEleccionColoresPersonalizados();
 
     }
 }

4c6c1e4b2f1fc3f2147df7ee8a550734ca3b44ae
Patricio <pantinao.guairao@gmail.com>
2018-08-08 21:55:46 -0400

    Cambios en la instancia.

--- src/main/java/Principal/Aplicacion.java
+++ src/main/java/Principal/Aplicacion.java
@@ -1,6 +1,7 @@
 
 package Principal;
 
+import Visual.Ventana;
 import Visual.VentanaEleccionColoresPersonalizados;
 import Visual.VentanaInicioSesion;
 
@@ -8,10 +9,7 @@ public class Aplicacion {
     public static void main(String[] args) {
 
         VentanaInicioSesion vInSe = new VentanaInicioSesion();
-
-        //Ventana ventana = new Ventana();
-        //VentanaInicioSesion vInSe = new VentanaInicioSesion();
-        VentanaEleccionColoresPersonalizados veColores = new VentanaEleccionColoresPersonalizados();
+        //VentanaEleccionColoresPersonalizados veColores = new VentanaEleccionColoresPersonalizados();
 
     }
 }
